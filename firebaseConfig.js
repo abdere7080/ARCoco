f1840ded847b4f8fd50c993827853c30df9efb94
abdere7080 <abderezk7080@gmail.com>
2025-12-01 00:33:56 +0100

    Add Firebase configuration file
    This file contains the Firebase configuration settings for the project, including API keys and other identifiers.

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+// public/firebaseConfig.js
+// إعدادات مشروع Firebase (باستخدام compat SDK)
+
+// ⚠️ تأكد أن هذا الملف محمّل قبل app.js في index.html
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyDG568OW2GtJG5MT2pvclEkN9yisENTIWQ",
+  authDomain: "tury-ab380.firebaseapp.com",
+  databaseURL: "https://tury-ab380-default-rtdb.firebaseio.com",
+  projectId: "tury-ab380",
+  storageBucket: "tury-ab380.firebasestorage.app",
+  messagingSenderId: "834477672073",
+  appId: "1:834477672073:web:9347ad15a5a3b78101a858",
+  measurementId: "G-M9L6BCENB2"
+};
